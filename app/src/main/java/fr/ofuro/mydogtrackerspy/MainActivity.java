15e372fe454c187b50bafa2c82bb4eceeb3141db
ofuro88 <guiffan.florestan@gmail.com>
2017-12-21 05:38:04 +0100

    first commit

---
+++ app/src/main/java/fr/ofuro/mydogtrackerspy/MainActivity.java
@@ -0,0 +1,13 @@
+package fr.ofuro.mydogtrackerspy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
